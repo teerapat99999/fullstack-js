fa585826b761f625ba232bbdd3acc8debbf94d61
TEERAPAT CHUCHUAY <teerapatchuchuay4240@gmail.com>
2024-04-22 07:49:02 +0700

    update
    bootstrap

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import { Row, Container,Col,Card,Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [user, setuser] = useState([]);
+
+  
+  useEffect(() => {
+    fetch("http://localhost:3333/user")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setuser(result);
+        }
+      )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+       <Container>
+        <Row>
+          {user.map(user => ( 
+            <Col xs = {12} sm = {6}  key = {user.id} >
+            <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{user.username_user}</Card.Title>
+          <Card.Text>
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+            </Col>
+            ))}
+          
+        </Row>
+       </Container>
     </div>
   );
 }
